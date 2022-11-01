7b459cd7edb793cd10bead9de659dd4d34044355
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2022-11-01 14:33:09 -0300

    Please, do something

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -33,3 +33,8 @@ worker.addEventListener('activate', (event) => {
 		})
 	);
 });
+
+worker.addEventListener('fetch', (e) => {
+	console.log(e.request.url);
+	e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+});
