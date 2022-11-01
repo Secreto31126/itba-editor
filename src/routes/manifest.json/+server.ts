4dbf4e355a57c172983258d3b2577ab7ea3863b7
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2022-11-01 13:12:04 -0300

    Test 6

--- src/routes/manifest.json/+server.ts
+++ src/routes/manifest.json/+server.ts
@@ -1,7 +1,32 @@
 import type { RequestHandler } from './$types';
 
-import manifest from '$lib/manifest.json?raw';
-
 export const GET: RequestHandler = async () => {
-	return new Response(manifest);
+	return new Response('Hello world!');
 };
+
+// "shortcuts": [
+//     {
+//       "name": "How's weather today?",
+//       "short_name": "Today",
+//       "description": "View weather information for today",
+//       "url": "/today?source=pwa",
+//       "icons": [
+//         {
+//           "src": "/icons/logo_192.png",
+//           "sizes": "192x192"
+//         }
+//       ]
+//     },
+//     {
+//       "name": "How's weather tomorrow?",
+//       "short_name": "Tomorrow",
+//       "description": "View weather information for tomorrow",
+//       "url": "/tomorrow?source=pwa",
+//       "icons": [
+//         {
+//           "src": "/icons/logo_192.png",
+//           "sizes": "192x192"
+//         }
+//       ]
+//     }
+//   ],
