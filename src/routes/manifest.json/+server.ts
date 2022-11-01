13912971b0278c86cccf55c8ba848917bb571f26
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2022-10-31 23:41:11 -0300

    Test 6

--- src/routes/manifest.json/+server.ts
+++ src/routes/manifest.json/+server.ts
@@ -1,8 +1,7 @@
 import type { RequestHandler } from './$types';
 
 import manifest from '$lib/manifest.json?raw';
-import { json } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async () => {
-	return json(manifest);
+	return new Response(manifest);
 };
