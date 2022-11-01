96335131d02e27e60f9b2185a65a5d6b741ae3a8
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2022-10-31 22:42:36 -0300

    Test 3

--- src/lib/Editor.svelte
+++ src/lib/Editor.svelte
@@ -2,10 +2,6 @@
 	import type monaco from 'monaco-editor';
 
 	import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
-	import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
-	import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
-	import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
-	import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 
 	import { onMount } from 'svelte';
 	import { browser } from '$app/environment';
@@ -21,23 +17,7 @@
 	onMount(async () => {
 		self.MonacoEnvironment = {
 			getWorker: function (_moduleId, label) {
-				switch (label) {
-					case 'json':
-						return new jsonWorker();
-					case 'css':
-					case 'scss':
-					case 'less':
-						return new cssWorker();
-					case 'html':
-					case 'handlebars':
-					case 'razor':
-						return new htmlWorker();
-					case 'typescript':
-					case 'javascript':
-						return new tsWorker();
-					default:
-						return new editorWorker();
-				}
+				return new editorWorker();
 			}
 		};
 
